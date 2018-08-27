43b7797eda0badb8b22b76f9494c5e2225759760
StanislavAntunovich <stanislav.antunovich@yandex.ru>
2018-08-27 22:32:39 +0300

    list removed

--- task_08.py
+++ task_08.py
@@ -1,21 +1,20 @@
 # Посчитать, сколько раз встречается определенная цифра в введенной последовательности чисел.
 # Количество вводимых чисел и цифра, которую необходимо посчитать, задаются вводом с клавиатуры.
 
-num_list = []
+nums = ''
 
 while True:
     num = input('Введите число (0 для завершения): ')
     if num == '0':
         break
-    num_list.append(num)
+    nums += num
 
 digit_to_count = input('Введите цифру для подсчета: ')
 
 digit_count = 0
-num_list = ''.join(num_list)
 
-for digit in num_list:
+for digit in nums:
     if digit == digit_to_count:
         digit_count += 1
 
-print(f'Цифра "{digit_to_count}" встречается {num_list.count(digit_to_count)} раз')
+print(f'Цифра "{digit_to_count}" встречается {nums.count(digit_to_count)} раз')

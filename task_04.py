7083f0a2252422cc27dec6bb26e9757c550df007
StanislavAntunovich <39097798+StanislavAntunovich@users.noreply.github.com>
2018-08-28 10:09:06 +0000

    Update task_04.py
    recursion variant added

--- task_04.py
+++ task_04.py
@@ -10,3 +10,14 @@ while range_count > 0:
     range_count -= 1
 
 print(f'Сумма элементов ряда равна {items_sum}')
+
+
+# Вариант 2
+def items_summ_count(item_index, start_item=1):
+    if item_index <= 1:
+        return start_item
+    else:
+        return start_item + items_summ_count(item_index - 1, start_item / -2)
+    
+    
+ print(items_summ_count(5))

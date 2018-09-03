d720e224f4b3f5bea25c1faf314ff03b4869b19f
StanislavAntunovich <stanislav.antunovich@yandex.ru>
2018-09-04 01:17:26 +0300

    keep working

--- lesson_04/task_01.py
+++ lesson_04/task_01.py
@@ -1,6 +1,6 @@
 # Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...Количество элементов (n) вводится с клавиатуры.
 
-import cProfile
+# import cProfile
 import functools
 
 
@@ -12,9 +12,13 @@ def items_sum_loop(range_count):
         item /= -2
         range_count -= 1
     return items_sum
+# 100 loops, best of 3: 1.5 usec per loop - 10 раз
+# 100 loops, best of 3: 2.73 usec per loop - 20 раз
+# 100 loops, best of 3: 5.2 usec per loop - 40 раз
 
-
-cProfile.run('items_sum_loop(50)')
+# cProfile.run('items_sum_loop(10)')
+# cProfile.run('items_sum_loop(20)')
+# cProfile.run('items_sum_loop(40)')
 
 
 def items_sum_recursion(range_count, current_item=1.0):
@@ -23,9 +27,13 @@ def items_sum_recursion(range_count, current_item=1.0):
     elif range_count == 1:
         return current_item
     return current_item + items_sum_recursion(range_count - 1, current_item / -2)
+# 100 loops, best of 3: 2.5 usec per loop - 10
+# 100 loops, best of 3: 4.78 usec per loop - 20
+# 100 loops, best of 3: 9.39 usec per loop - 40
 
-
-cProfile.run('items_sum_recursion(50)')
+# cProfile.run('items_sum_recursion(10)')
+# cProfile.run('items_sum_recursion(20)')
+# cProfile.run('items_sum_recursion(40)')
 
 
 def items_sum_m(range_count):
@@ -38,9 +46,14 @@ def items_sum_m(range_count):
         return func_dict[_range_count]
 
     return _items_sum(range_count)
+# 100 loops, best of 3: 3.28 usec per loop - 10
+# 100 loops, best of 3: 6.04 usec per loop - 20
+# 100 loops, best of 3: 12.2 usec per loop - 40
 
 
-cProfile.run('items_sum_m(50)')
+# cProfile.run('items_sum_m(10)')
+# cProfile.run('items_sum_m(20)')
+# cProfile.run('items_sum_m(40)')
 
 
 @functools.lru_cache()
@@ -50,6 +63,10 @@ def items_sum_c(range_count, current_item=1.0):
     elif range_count == 1:
         return current_item
     return current_item + items_sum_c(range_count - 1, current_item / -2)
+# 100 loops, best of 3: 0.147 usec per loop - 10
+# 100 loops, best of 3: 0.147 usec per loop - 20
+# 100 loops, best of 3: 0.14 usec per loop - 40
 
-
-cProfile.run('items_sum_c(50)')
+# cProfile.run('items_sum_c(10)')
+# cProfile.run('items_sum_c(20)')
+# cProfile.run('items_sum_c(40)')

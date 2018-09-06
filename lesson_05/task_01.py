7ef69292ab6b99714ff6dd945ed77714a0b7f604
StanislavAntunovich <stanislav.antunovich@yandex.ru>
2018-09-06 22:52:58 +0300

    а

--- lesson_05/task_01.py
+++ lesson_05/task_01.py
@@ -2,25 +2,23 @@
 # Программа должна определить среднюю прибыль (за год для всех предприятий) и вывести наименования предприятий,
 # чья прибыль выше среднего и отдельно вывести наименования предприятий, чья прибыль ниже среднего.
 
-import collections
-
 firms_count = int(input('Введите количество предпиятий: '))
-firms = {}
+firms_profit = {}
 total_profit = 0
 
 for firm in range(firms_count):
     firm_name = input(f'Введите название для {firm + 1} предпиятия: ')
-    firms[firm_name] = 0
+    firms_profit[firm_name] = 0
 
     for quoter in range(4):
         profit = float(input(f'Введите прибыль за {quoter + 1} квартал: '))
         total_profit += profit
-        firms[firm_name] += profit
+        firms_profit[firm_name] += profit
 
 average_profit = total_profit / firms_count
 
-lower_profit = [f for f in firms if firms[f] < average_profit]
-higher_profit = [f for f in firms if firms[f] > average_profit]
+lower_profit = [f for f in firms_profit if firms_profit[f] < average_profit]
+higher_profit = [f for f in firms_profit if firms_profit[f] > average_profit]
 
 print('Фирмы с прибылью выше среднего: ', end='')
 print(*higher_profit, sep=', ')

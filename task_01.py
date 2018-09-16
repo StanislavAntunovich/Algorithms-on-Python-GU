d144f14e738f0dea2b9aa96d10e915b41d595586
StanislavAntunovich <stanislav.antunovich@yandex.ru>
2018-09-16 19:40:46 +0300

    d_c

---
+++ task_01.py
@@ -0,0 +1,22 @@
+# Определение количества различных подстрок с использованием хеш-функции. Пусть дана строка S длиной N.
+# Например, состоящая только из маленьких латинских букв. Требуется найти количество различных подстрок в этой строке.
+# Для решения задачи рекомендую воспользоваться алгоритмом sha1 из модуля hashlib.
+
+from hashlib import sha1
+
+string = input('Введите строку: ')
+
+str_len = len(string)
+sub_len = 1
+
+sub_strings = []
+
+while str_len > 1:
+    for i in range(str_len):
+        sub = sha1(string[i:i + sub_len].encode('utf-8')).hexdigest()
+        if sub not in sub_strings:
+            sub_strings.append(sub)
+    sub_len += 1
+    str_len -= 1
+
+print(len(sub_strings))
